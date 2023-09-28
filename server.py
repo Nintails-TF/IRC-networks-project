8bf9f70d387a3fa9a05514d8a32db1795b8949aa
Finnbar Home <finnbar.home2@gmail.com>
2023-09-28 17:34:10 +0100

    Added a dictionary for remembering client info

--- server.py
+++ server.py
@@ -1,20 +1,26 @@
 import socket
 
-# define server constants
+#Define server constants
 HOST = '::'  # listen on all IPv6 addresses
 PORT = 6667
 
-# create the socket
+#Create the socket
 s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
-# bind the socket to the host and the port
+#Bind the socket to the host and the port
 s.bind((HOST, PORT))
 
-# listen for incoming connections 
+#Listen for incoming connections 
 s.listen(5)
 print(f"Listening on {HOST} : {PORT}")
 
+
+clients_info = {}
+
 while True:
-    # accept incoming connections
+    #Accept incoming connections
     client_socket, client_address = s.accept()
     print(f"Accepted connection from {client_address[0]} : {client_address[1]}")
+    
+    #Remember client's info
+    clients_info[client_socket] = {"address": client_address}
