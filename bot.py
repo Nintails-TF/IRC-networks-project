aa642ee86e835b94c1b2a5e660162c7b592e3d47
Lewis Simmonds <LCSimmonds@dundee.ac.uk>
2023-10-12 16:54:10 +0100

    cli stuff

--- bot.py
+++ bot.py
@@ -1,4 +1,12 @@
 import socket
+import argparse
+
+# Initialize the default values for host, port, realname, nickname, and channel
+host = "::1"
+port = 6667
+realname = "Swag"
+nickname = "SwagBot"
+channel = "#test"
 
 """
 The socket class is responsible for handling the network connections between the Bot (client) and
@@ -6,97 +14,94 @@ the server.
 """
 class Socket:
     def __init__(self, host, port):
-        self.host = host 
-        self.port = port 
+        self.host = host
+        self.port = port
 
-    def connectToServer(self):
-        # Setting the NICK and REAL name of the bot
-        swagBot = Bot("SwagBot", "Swag")
-        # Defining a socket, with Ipv6 using TCP socket
+    def connectToServer(self, bot):
         with socket.socket(socket.AF_INET6, socket.SOCK_STREAM) as s:
             s.connect((self.host, self.port))
-            s.send(swagBot.botRegistration()) # Send NICK and USER details
-            # RETAIN INFO STEP - use recv to get data from IRC server
-            s.send(swagBot.botJoinChannel())
+            s.send(bot.botRegistration())
+            s.send(bot.botJoinChannel())
             self.keepalive(s)
 
-    # keepalive will keep the bot in the IRC server
     def keepalive(self, s):
-        # This will loop until you CTRL+C
         while True:
             try:
-                # The response is the text that the bot gets from the server, we now need to parse it to perform actions.
                 response = s.recv(2048).decode()
-                print(response) # Printing out response for testing
-                # If we see PING request
+                print(response)
                 if response.startswith("PING"):
-                    self.pong(s, response) # Respond with pong
-                # ELIF we can get the userlist if we see the 353 IRC code.
+                    self.pong(s, response)
                 elif "353" in response:
                     self.userlist(s, response)
             except KeyboardInterrupt:
                 break
 
-    # pong will handle ping requests with a corresponding pong
     def pong(self, s, text):
-        # Extract the PING message
         ping_message = text.split(" ")[1]
-        # Send a PONG response back to the server
         response = "PONG " + ping_message + "\r\n"
         s.send(response.encode())
 
-    # userlist will grab the initial userlist and store it.
     def userlist(self, s, text):
-        print(text.split("353")) # testing output
+        print(text.split("353"))
 
     def getHost(self):
         return self.host
-    
+
     def setHost(self, host):
         self.host = host
 
     def getPort(self):
         return self.port
-    
+
     def setPort(self, port):
         self.port = port
 
-
 """
-The Menu class is resonsible for handling user input via the CLI/terminal, ensuring that
+The Menu class is responsible for handling user input via the CLI/terminal, ensuring that
 users can input flags to modify parameters. e.g.
 
-python bot.py --ipv6 fc00:1337::19
+python bot.py --host fc00:1337::19
 
-would set the Ipv6 address to connect to.
+would set the IPv6 address to connect to.
 """
 class Menu:
     def __init__(self) -> None:
-        pass
+        self.parser = argparse.ArgumentParser(description="IRC Bot Configuration")
+        self.parser.add_argument('--host', default=host, help="IPv6 address to connect to")
+        self.parser.add_argument('--port', type=int, default=port, help="Port to connect to")
+        self.parser.add_argument('--nickname', default=nickname, help="Nickname of the bot")
+        self.parser.add_argument('--realname', default=realname, help="Real name of the bot")
+        self.parser.add_argument('--channel', default=channel, help="Channel to join")
+
+    def parse_arguments(self):
+        return self.parser.parse_args()
 
 """
-The Bot class is responsible for holding all the functions that the bot must perform. e.g. getting registeration details, 
+The Bot class is responsible for holding all the functions that the bot must perform. e.g. getting registration details, 
 sending messages, etc.
 """
 class Bot:
-    def __init__(self, nickname, realname):
-        self.nickname = nickname 
-        self.realname = realname 
+    def __init__(self, nickname, realname, channel):
+        self.nickname = nickname
+        self.realname = realname
+        self.channel = channel
 
     # @return a formatted NICK and USER command
     def botRegistration(self):
-        user = "NICK " + self.nickname +  "\r\nUSER " + self.nickname + " 0 * " + ":" + self.realname +"\r\n"
-        return user.encode() 
-    
+        user = "NICK " + self.nickname + "\r\nUSER " + self.nickname + " 0 * " + ":" + self.realname + "\r\n"
+        return user.encode()
+
     # @return a formatted join statement to join the test channel.
     def botJoinChannel(self):
-        join = "JOIN #test\r\n"
+        join = "JOIN " + self.channel + "\r\n"
         return join.encode()
 
 def main():
-    # CHECK FOR USER INPUTS
-    clientSocket = Socket("fc00:1337::17", 6667) # Linux IP - fc00:1337::17, Localhost = ::1, Windows IP - fc00:1337::19
-    clientSocket.connectToServer() 
+    menu = Menu()
+    args = menu.parse_arguments()
+    clientSocket = Socket(args.host, args.port)
+    swagBot = Bot(args.nickname, args.realname, args.channel)
+    clientSocket.connectToServer(swagBot)
 
 if __name__ == "__main__":
     main()
